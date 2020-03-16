88db4c41c5c2e949df81cf2510f6c2b0d705f398
Ammar Razzak <nanoproductions@Family-iMac.local>
2020-03-16 17:24:25 -0400

    Fixed Imports

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Layout from '../components/layout/Layout';
+import { Helmet } from 'react-helmet';
 
 const About = () => {
   return (
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Layout from '../components/layout/Layout';
 import Showcase from '../components/layout/Showcase';
 import LatestPosts from '../components/LatestPosts';
+import { Helmet } from 'react-helmet';
 
 export default ({ data }) => {
   return (
