cdfd63daafdeefb6099bcaef91978b937d96085a
Nano Adam <nanoproductions@Family-iMac.local>
2020-04-05 21:35:05 -0400

    Removed Break

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,8 +2,5 @@ import { wrapRootElement as wrap } from './root-wrapper';
 import 'jquery/dist/jquery.min.js';
 import 'popper.js/dist/popper.min';
 import 'bootstrap/dist/js/bootstrap.min.js';
-import CMS from 'netlify-cms-app';
-
-CMS.registerPreviewStyle('./src/components/css/Admin-Preview.css');
 
 export const wrapRootElement = wrap;
