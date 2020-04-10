88b47627c81d8c10605ae9f12439e28c6f8cd9f5
Nano Adam <nanoproductions@Family-iMac.local>
2020-04-09 20:49:49 -0400

    Added G-Tracking

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,24 +1,31 @@
 module.exports = {
   siteMetadata: {
     title: 'Dev with Nano',
-    description: '...Learn Code'
+    description: '...Learn Code',
   },
   plugins: [
     {
       resolve: 'gatsby-plugin-mdx',
       options: {
-        extensions: ['.mdx', '.md']
-      }
+        extensions: ['.mdx', '.md'],
+      },
     },
     {
       resolve: 'gatsby-source-filesystem',
       options: {
         path: `${__dirname}/posts`,
-        name: 'posts'
-      }
+        name: 'posts',
+      },
     },
     `gatsby-plugin-sass`,
     `gatsby-plugin-netlify-cms`,
-    `gatsby-plugin-react-helmet`
-  ]
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: process.env.GOOGLE_TRACKING,
+        head: true,
+      },
+    },
+  ],
 };
