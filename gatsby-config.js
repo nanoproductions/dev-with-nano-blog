f3b3017637705c53cb54a4536690adbb19a41396
Nano Adam <nanoproductions@Family-iMac.local>
2020-05-05 18:25:09 -0400

    Default State

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,8 +22,10 @@ module.exports = {
       resolve: `gatsby-plugin-netlify-cms`,
       options: {
         modulePath: `${__dirname}/src/cms/cms.js`,
+        enableIdentityWidget: true,
       },
-    }`gatsby-plugin-react-helmet`,
+    },
+    `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
