d6c3ceab15391631528959f1715fdc5aa934566e
Nano Adam <nanoproductions2@gmail.com>
2020-07-02 14:50:02 -0400

    Fixed gatsby-browser.js

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,3 @@
 import { wrapRootElement as wrap } from './root-wrapper'
-import 'jquery/dist/jquery.min.js'
-import 'popper.js/dist/popper.min'
-import 'bootstrap/dist/js/bootstrap.min.js'
 
 export const wrapRootElement = wrap
