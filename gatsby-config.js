8101346264d5acf46e5dba0c952e5e01f94015e9
Nano Adam <nanoproductions2@gmail.com>
2020-09-22 19:05:05 -0400

    Minor issue 'node' vs 'nodes' in graphql query - fixed

--- gatsby-config.js
+++ gatsby-config.js
@@ -101,7 +101,7 @@ module.exports = {
                   sort: { order: DESC, fields: [frontmatter___date] },
                   filter: {frontmatter: {published: {eq: true}}}
                 ) {
-                    node {
+                    nodes {
                       excerpt
                       html
                       fields { slug }
